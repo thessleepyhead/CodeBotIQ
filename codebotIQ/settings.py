6c7d4c92305cabedcc87e0e15ab7ba6bc448290a
Archana Mishra <107236325+thessleepyhead@users.noreply.github.com>
2025-07-10 23:36:45 +0530

    Update settings.py

--- codebotIQ/settings.py
+++ codebotIQ/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q43^et&p#riy@m_caiyy$umcb1tjb!=81f^#na4mk!=z*f4k-7'
+SECRET_KEY = 'your security key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
